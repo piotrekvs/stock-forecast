116a2c0859d62fcebd13ce1f0599455344bae35a
piotrekvs <pvanselow.2000@gmail.com>
2025-08-25 17:18:22 +0200

    add new datasets and gridsearch notebook

--- lib/preprocessing/normalization.py
+++ lib/preprocessing/normalization.py
@@ -8,31 +8,29 @@ from typing import Sequence
 
 
 class Normalizer():
-    def __init__(self, diff=False):
-        log_mapper = InvertibleMapper(
+    def __init__(self):
+        self.log_mapper = InvertibleMapper(
             fn=np.log,
             inverse_fn=np.exp, # type: ignore
             name="Log"
         )
+        self.pipeline = Pipeline([self.log_mapper])
 
-        self.pipeline = Pipeline([log_mapper, Diff(lags=1)] if diff else [log_mapper])
-
-    def log_normalize(self, series: TimeSeries) -> TimeSeries | Sequence[TimeSeries]:
+    def log_normalize(self, series: TimeSeries):
         return self.pipeline.fit_transform(series)
     
-    def inverse_log_normalize(self, series: TimeSeries) -> TimeSeries | Sequence[TimeSeries]:
+    def inverse_log_normalize(self, series: TimeSeries):
         return self.pipeline.inverse_transform(series)
 
-class Scalers():
+class MyScaler():
     def __init__(self):
-        # self.scaler = Scaler(MinMaxScaler(feature_range=(0, 1)))
-        self.scaler = Scaler(StandardScaler())
+        self.scaler = Scaler(MinMaxScaler(feature_range=(0, 1)))
 
-    def fit_scaler(self, series: TimeSeries | Sequence[TimeSeries]) -> TimeSeries | Sequence[TimeSeries]:
+    def fit_transform(self, series: TimeSeries | Sequence[TimeSeries]):
         return self.scaler.fit_transform(series)
 
-    def transform(self, series: TimeSeries | Sequence[TimeSeries]) -> TimeSeries | Sequence[TimeSeries]:
+    def transform(self, series: TimeSeries | Sequence[TimeSeries]):
         return self.scaler.transform(series)
 
-    def inverse_scaler(self, series: TimeSeries | list[TimeSeries] | list[list[TimeSeries]]) -> (TimeSeries | list[TimeSeries] | list[list[TimeSeries]]):
+    def inverse(self, series: TimeSeries | list[TimeSeries] | list[list[TimeSeries]]):
         return self.scaler.inverse_transform(series)
